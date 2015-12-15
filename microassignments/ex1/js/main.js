66dd89ec5fd2fd45d6c1d400af7c9e60ea863174
Roxana Babatie <roxana@Macs-MacBook-Pro.local>
2015-12-15 12:24:58 +0200

    final

---
+++ microassignments/ex1/js/main.js
@@ -0,0 +1,16 @@
+var value;
+var go = document.getElementById("progr");
+var button;
+
+ var getRandomInt = function (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+var listener = function () {
+	go.value = getRandomInt(1, 100);
+};
+
+
+
+button = document.getElementById("btn");
+button.addEventListener("click", listener);
