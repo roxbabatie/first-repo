7b09eb3127294817633fca6b2a47ded4f8a6fd7c
Roxana Babatie <roxana@Macs-MacBook-Pro.local>
2015-12-17 12:49:52 +0200

    event listener on all inputs and validation

--- microassignments/ex8/js/main.js
+++ microassignments/ex8/js/main.js
@@ -2,36 +2,54 @@ var selectOperator = document.getElementById("sel-op");
 var aBox = document.getElementById("abox");
 var bBox = document.getElementById("bbox");
 var res = document.getElementById("resultbox");
+var x;
+var a;
+var b;
+var result;
 
+
+function go(){
+	x = selectOperator.value;
+	a = aBox.value;
+	b = bBox.value;
+	result = res.value;
+
+	if ((a == "") || (b == "")){
+		result = "";
+	}
+	else {
+		switch (x) {
+			case "add":
+					res.value = parseFloat(a) + parseFloat(b);
+					break;
+			case "substract":
+					res.value = parseFloat(a) - parseFloat(b);
+					break;
+			case "multiply":
+					res.value = parseFloat(a) * parseFloat(b);
+					break;
+			case "divide":
+					if (parseFloat(b) == 0) {
+							res.value = "NaN";
+					}
+					else {
+							res.value = parseFloat(a) / parseFloat(b);
+					}
+					break;
+		}
+	}
+}
 
 selectOperator.addEventListener("change", function(event) {
-	var x = selectOperator.value;
-	console.log(x);
-	var a = aBox.value;
-	var b = bBox.value;
-	var result = res.value;
-	
-	switch (x) {
-		case "add":
-				res.value = parseFloat(a) + parseFloat(b);
-				break;
-		case "substract":
-				res.value = parseFloat(a) - parseFloat(b);
-				break;
-		case "multiply":
-				res.value = parseFloat(a) * parseFloat(b);
-				break;
-		case "divide":
-				if (parseFloat(b) == 0) {
-						res.value = "NaN";
-				}
-				else {
-						res.value = parseFloat(a) / parseFloat(b);
-				}
-				break;
+		go();
 
-	}
+});
+aBox.addEventListener("input", function(event) {
+		go();
 
+});
+bBox.addEventListener("input", function(event) {
+		go();
 
 });
 
