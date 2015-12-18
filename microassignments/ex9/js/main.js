37c9d33f1fddb92f2fe7f60812b15cf4b3bc57e4
Roxana Babatie <roxana@Macs-MacBook-Pro.local>
2015-12-18 13:48:28 +0200

    all done except year options

--- microassignments/ex9/js/main.js
+++ microassignments/ex9/js/main.js
@@ -1,45 +1,86 @@
-var d = document.getElementById("day");
-var m = document.getElementById("month");
-var y = document.getElementById("year");
+var day = document.getElementById("day");
+var month = document.getElementById("month");
+var year = document.getElementById("year");
 var container = document.getElementById("container");
 var birthday;
 
-y.addEventListener("change", function(event){
-		birthday = new Date(y.value,m.value,d.value);
-		console.log(birthday.toDateString());
+function populateSelect(target, min, max){
+	if (!target){
+		return false;
+	}
+	else {
+		var min = min || 1;
+		var	max = max || min;
+		select = document.getElementById(target);
+		for (var i = min; i<=max; i++){
+			var opt = document.createElement('option');
+			opt.value = i;
+			opt.innerHTML = i;
+			select.appendChild(opt);
+		}
+	}
+}
+
+populateSelect('day',1,31);
+populateSelect('month',1,12);
+populateSelect('year',1940, 2015);
+
+function validInputs() {
+	return !((year.value == "default") || (month.value == "default") || (day.value == "default"));
+}
+
+year.addEventListener("change", function(event){
+	if (validInputs()) {
+		birthday = new Date(year.value, parseInt(month.value) - 1, day.value);
+		//console.log(birthday.toDateString());
 		checkAge(birthday);
+	}
 });
 
-m.addEventListener("change", function(event){
-		birthday = new Date(y.value,m.value,d.value);
+month.addEventListener("change", function(event){
+	if (validInputs()) {
+		birthday = new Date(year.value,parseInt(month.value)-1,day.value);
 		checkAge(birthday);
-		console.log(birthday.toDateString());
+	}
 });
 
+day.addEventListener("change", function(event){
+	birthday = new Date(year.value,parseInt(month.value)-1,day.value);
+	if (validInputs()) {
 
-d.addEventListener("change", function(event){
-		birthday = new Date(y.value,m.value,d.value);
 		checkAge(birthday);
-		console.log(birthday.toDateString());
+	}
 });
 
+function checkAge(bday) {
+	var eighteenYearsAgo = new Date();
 
-function checkAge(bday)
-{
-  var eighteenYearsAgo = new Date();
+  	eighteenYearsAgo.setYear(eighteenYearsAgo.getFullYear() - 18);
+	eighteenYearsAgo.setHours(0);
+  	eighteenYearsAgo.setMinutes(0);
+  	eighteenYearsAgo.setSeconds(1);
 
-  eighteenYearsAgo.setYear(eighteenYearsAgo.getFullYear() - 18);
-  eighteenYearsAgo.setHours(0);
-  eighteenYearsAgo.setMinutes(0);
-  eighteenYearsAgo.setSeconds(1);
+  	if (bday.getTime() > eighteenYearsAgo.getTime()) {
+    	console.log("You must be at least 18 years of age.");
+		container.classList.add("under");
+		var a = document.createElement('a');
+		var linkText = document.createTextNode("Go to Disney.com");
+		a.appendChild(linkText);
+		a.title = "my title text";
+		a.href = "http://disney.com";
+		container.appendChild(a);
 
-  if (bday.getTime() > eighteenYearsAgo.getTime()) {
-
-    console.log("You must be at least 18 years of age.");
-  } else {
-  	container.classList.add("over");
-  	console.log("good enough");
-  }
+  	}
+	else {
+		container.classList.add("over");
+		var a = document.createElement('a');
+		var linkText = document.createTextNode("Go to Disney.com anyways");
+		a.appendChild(linkText);
+		a.title = "my title text";
+		a.href = "http://disney.com";
+		container.appendChild(a);
+  		console.log("good enough");
+  	}
 }
 
 
