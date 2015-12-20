4ae2b285f6b10eca096ce18f062096d2a5aed1c4
Roxana Babatie <roxana@Macs-MacBook-Pro.local>
2015-12-20 20:21:01 +0200

    add no of stars in table

--- microassignments/bigassign/js/main.js
+++ microassignments/bigassign/js/main.js
@@ -1,6 +1,24 @@
+
 var myForm = document.getElementById("my-form");
 var myTable = document.getElementById("my-table");
 var tableBody = myTable.getElementsByTagName("tbody")[0];
+
+var stars = document.getElementsByClassName('star');
+var theCounting = document.getElementById('counter');
+var length;
+
+for (var i = 0; i < stars.length; i++) {
+    var star = stars[i];
+
+    star.addEventListener("click", function() {
+        var counting = this.getAttribute("data-value");
+        theCounting.innerHTML = counting;
+        length = parseInt(counting, 10);
+        for (var j = 0; j < length; j++) {
+            stars[j].classList.add('active');
+        };
+    });
+}
 
 function getValues(form) {
     var inputs = form.getElementsByTagName("input");
@@ -8,7 +26,8 @@ function getValues(form) {
     var city = inputs[1].value;
     return {
         name: name,
-        city: city
+        city: city,
+        stars: length
     };
 }
 
